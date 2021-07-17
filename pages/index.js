57f385524e09abafa880c5a3c71912a4eedbeb8e
Aleksandar Koprivica <aleksandar.kop@gmail.com>
2021-07-17 10:24:39 +0200

    website is coming soon

--- pages/index.js
+++ pages/index.js
@@ -2,64 +2,74 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>WATFOI - New website coming soon</title>
+                <link rel="icon" href="/favicon.ico"/>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+                <title>We are the future of IT - Novi veb-sajt stiže brže nego što misliš</title>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+                <title>We are the future of IT - Novi veb-sajt stiže brže nego što misliš</title>
+                <meta name="title" content="We are the future of IT - Novi veb-sajt stiže brže nego što misliš"/>
+                <meta name="description"
+                      content="Studentska IT konferencija We are the FUTURE of IT / 11-14. oktobar 2021 / Studentsko odrmaralište Ratko Mitrović, Zlatibor. Vidimo se!"/>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://wearethefutureofit.org/"/>
+                <meta property="og:title" content="We are the future of IT - Novi veb-sajt stiže brže nego što misliš"/>
+                <meta property="og:description"
+                      content="Studentska IT konferencija We are the FUTURE of IT / 11-14. oktobar 2021 / Studentsko odrmaralište Ratko Mitrović, Zlatibor. Vidimo se!"/>
+                <meta property="og:image"
+                      content="/watfoi.png"/>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content="https://wearethefutureofit.org/"/>
+                <meta property="twitter:title"
+                      content="We are the future of IT - Novi veb-sajt stiže brže nego što misliš"/>
+                <meta property="twitter:description"
+                      content="Studentska IT konferencija We are the FUTURE of IT / 11-14. oktobar 2021 / Studentsko odrmaralište Ratko Mitrović, Zlatibor. Vidimo se!"/>
+                <meta property="twitter:image"
+                      content="/watfoi.png"/>
+            </Head>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    <img src="/logo.png" alt="Vercel Logo" className={styles.watfoilogo}/>
+                </h1>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <p className={styles.description}>
+                    Naš novi veb-sajt stiže brže nego što misliš 🚀
+                </p>
+
+                <p className={styles.description}>
+                    Do tada nas možeš pratiti na:
+                </p>
+                <div className={styles.grid}>
+                    <a href="https://www.facebook.com/watfoi/" target="_blank" className={styles.card}>
+                        <h3>Facebook</h3>
+                    </a>
+
+                    <a href="https://www.instagram.com/wearethefutureofit/" target="_blank" className={styles.card}>
+                        <h3>Instagram</h3>
+                    </a>
+
+                    <a href="https://www.linkedin.com/company/wearethefutureofit" target="_blank" className={styles.card}>
+                        <h3>Linkedin</h3>
+                    </a>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by WATFOI & {' '}
+                    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
+                </a>
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
